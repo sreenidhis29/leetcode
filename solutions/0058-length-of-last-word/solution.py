aac74482baddcf7993d7ec0764ffd581cedf3858
Shreenidhi S <165373137+sreenidhis29@users.noreply.github.com>
2025-08-06 06:56:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.6 MB (16.28%)

---
+++ solutions/0058-length-of-last-word/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        i = len(s) - 1
+        length = 0
+
+        # Skip trailing spaces
+        while i >= 0 and s[i] == ' ':
+            i -= 1
+
+        # Count characters of the last word
+        while i >= 0 and s[i] != ' ':
+            length += 1
+            i -= 1
+
+        return length
+
