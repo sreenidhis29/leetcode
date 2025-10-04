8e104bdc7e9bb900dd7aabf17a2d78744a5e9800
Shreenidhi S <165373137+sreenidhis29@users.noreply.github.com>
2025-10-04 02:01:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

---
+++ solutions/1642-water-bottles/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def numWaterBottles(self, numBottles, numExchange):
+        total = numBottles
+        empty = numBottles
+
+        while empty >= numExchange:
+            new_full = empty // numExchange
+            total += new_full
+            empty = empty % numExchange + new_full
+
+        return total
