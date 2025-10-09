a1b11e3daaaa188ca1115ea68f433fbafddc314a
Shreenidhi S <165373137+sreenidhis29@users.noreply.github.com>
2025-10-09 00:57:04 +0000

    Sync LeetCode submission Runtime - 25 ms (95.88%), Memory - 92.4 MB (100.00%)

---
+++ solutions/0380-insert-delete-getrandom-o1/solution.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+class RandomizedSet {
+    private HashMap<Integer, Integer> map;
+    private ArrayList<Integer> list;
+    private Random rand;
+
+    public RandomizedSet() {
+        map = new HashMap<>();
+        list = new ArrayList<>();
+        rand = new Random();
+    }
+
+    public boolean insert(int val) {
+        if (map.containsKey(val)) return false;
+        map.put(val, list.size());
+        list.add(val);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) return false;
+        int index = map.get(val);
+        int lastElement = list.get(list.size() - 1);
+        list.set(index, lastElement);
+        map.put(lastElement, index);
+        list.remove(list.size() - 1);
+        map.remove(val);
+        return true;
+    }
+
+    public int getRandom() {
+        return list.get(rand.nextInt(list.size()));
+    }
+}
