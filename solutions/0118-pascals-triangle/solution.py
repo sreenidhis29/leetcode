4fe753186348dc428dc27b8257b214b16cf9a063
Shreenidhi S <165373137+sreenidhis29@users.noreply.github.com>
2025-11-15 16:28:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18 MB (100.00%)

---
+++ solutions/0118-pascals-triangle/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def generate(self, numRows):
+        res = []
+        for i in range(numRows):
+            row = [1] * (i + 1)
+            for j in range(1, i):
+                row[j] = res[-1][j - 1] + res[-1][j]
+            res.append(row)
+        return res
+
