a6c5dd75d8054f99e513ca6c0f99541e6c3d13c8
Shreenidhi S <165373137+sreenidhis29@users.noreply.github.com>
2025-11-22 15:36:54 +0000

    Sync LeetCode submission Runtime - 8 ms (90.45%), Memory - 49.3 MB (13.05%)

---
+++ solutions/0056-merge-intervals/solution.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1) {
+            return intervals;
+        }
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        List<int[]> merged = new ArrayList<>();
+        int[] currentInterval = intervals[0];
+        merged.add(currentInterval);
+        for (int[] interval : intervals) {
+            int currentEnd = currentInterval[1];
+            int nextStart = interval[0];
+            int nextEnd = interval[1];
+
+            if (nextStart <= currentEnd) {
+                currentInterval[1] = Math.max(currentEnd, nextEnd);
+            } else {
+                currentInterval = interval;
+                merged.add(currentInterval);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
