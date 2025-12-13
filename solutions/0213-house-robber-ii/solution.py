b1eb29a7ce933fe9e2c232bc82298d7bef706a9b
Shreenidhi S <165373137+sreenidhis29@users.noreply.github.com>
2025-12-13 15:28:40 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (100.00%)

---
+++ solutions/0213-house-robber-ii/solution.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        def rob_linear(arr):
+            prev2 = prev1 = 0
+            for x in arr:
+                prev2, prev1 = prev1, max(prev1, prev2 + x)
+            return prev1
+        return max(rob_linear(nums[:-1]), rob_linear(nums[1:]))
