abb9ad1836e92a97e499a31223701885ceca2be1
Shreenidhi S <165373137+sreenidhis29@users.noreply.github.com>
2026-05-13 15:08:52 +0000

    Sync LeetCode submission Runtime - 199 ms (55.63%), Memory - 30.9 MB (95.28%)

---
+++ solutions/1793-minimum-moves-to-make-array-complementary/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minMoves(self, nums: List[int], limit: int) -> int:
+        n = len(nums)
+        dp = [0] * (2 * limit + 2)
+        for i in range(n // 2):
+            mini = min(nums[i], nums[-1 - i])
+            maxi = max(nums[i], nums[-1 - i])
+            dp[2] += 2
+            dp[mini + 1] -= 1
+            dp[mini + maxi] -= 1
+            dp[mini + maxi + 1] += 1
+            dp[maxi + limit + 1] += 1
+        res = n
+        moves = 0
+        for c in range(2, 2 * limit + 1):
+            moves += dp[c]
+            res = min(res, moves)
+        return res
