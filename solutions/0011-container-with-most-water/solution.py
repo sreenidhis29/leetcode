a4beab498f8d7b4be3d78015a8e03c933302d6e9
Shreenidhi S <165373137+sreenidhis29@users.noreply.github.com>
2026-07-03 16:43:54 +0000

    Sync LeetCode submission Runtime - 64 ms (17.43%), Memory - 29.6 MB (67.47%)

--- solutions/0011-container-with-most-water/solution.py
+++ solutions/0011-container-with-most-water/solution.py
@@ -1,21 +1,18 @@
-class Solution(object):
-    def maxArea(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        left, right = 0, len(height) - 1
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
         max_area = 0
 
         while left < right:
-            h = min(height[left], height[right])
-            w = right - left
-            max_area = max(max_area, h * w)
-
-            # Move the shorter line inward
+            width = right - left
+            current_height = min(height[left], height[right])
+            max_area = max(max_area, width * current_height)
+            
             if height[left] < height[right]:
                 left += 1
             else:
                 right -= 1
-
+        
         return max_area
+
