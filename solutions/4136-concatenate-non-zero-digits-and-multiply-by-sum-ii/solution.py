0262133815a4c78b6129ea695ecce3474c0b9da6
Shreenidhi S <165373137+sreenidhis29@users.noreply.github.com>
2026-07-08 17:17:06 +0000

    Sync LeetCode submission Runtime - 467 ms (69.23%), Memory - 56.8 MB (44.38%)

---
+++ solutions/4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/solution.py
@@ -0,0 +1,42 @@
+class Solution:
+    def sumAndMultiply(self, s: str, queries: List[List[int]]) -> List[int]:
+        MOD = 10**9 + 7
+        n = len(s)
+
+        pow10 = [1] * (n + 1)
+        for i in range(1, n + 1):
+            pow10[i] = (pow10[i - 1] * 10) % MOD
+        idx = [0] * (n + 1)
+        val = [0] * (n + 1)
+        total = [0] * (n + 1)
+
+        cnt = 0
+
+        for i, ch in enumerate(s):
+            digit = int(ch)
+
+            if digit != 0:
+                cnt += 1
+                val[cnt] = (val[cnt - 1] * 10 + digit) % MOD
+                total[cnt] = total[cnt - 1] + digit
+
+            idx[i + 1] = cnt
+
+        ans = []
+
+        for l, r in queries:
+
+            left = idx[l]
+            right = idx[r + 1]
+            if left == right:
+                ans.append(0)
+                continue
+
+            length = right - left
+
+            number = (val[right] - val[left] * pow10[length]) % MOD
+            sum_digits = total[right] - total[left]
+
+            ans.append((number * sum_digits) % MOD)
+
+        return ans
